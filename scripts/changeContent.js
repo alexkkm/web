0c362be12b8b1ac728ed9f8289a51f75e9d7be56
alexkkm <alexkong0222@gmail.com>
2021-08-17 16:43:59 +0800

    finish basic setting

--- scripts/changeContent.js
+++
@@ -1,5 +0,0 @@
-function changeContent()
-{
-    x=document.getElementsByID("Intro");
-    x.innerHTML="New Content Added";
-}
--- scripts/helloworld.js
+++
@@ -1,5 +0,0 @@
-function HelloWorldFunction()
-{
-    document.getElementById("p1").innerHTML="Changed content";
-}
-
